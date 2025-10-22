ea175794861d715cb7c71fac95d1eebc695aa268
rndmq <129494305+rndmq@users.noreply.github.com>
2025-10-22 19:12:39 +0700

    Create product.ts

---
+++ data/product.ts
@@ -0,0 +1,47 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Bola Futsal Pro',
+    price: {
+      4: 199000,
+      5: 249000,
+      6: 299000,
+    },
+    images: [
+     'https://cdn.discordapp.com/attachments/1419839079222218772/1430527475351556146/images_11.jpg?ex=68fa1a08&is=68f8c888&hm=ce875d65d886592c5b7b7c00d75c238b868fdbcb5bd1b38cbef7a3117a4e15f1&',
+     'https://cdn.discordapp.com/attachments/1419839079222218772/1430527475674255442/images_10.jpg?ex=68fa1a08&is=68f8c888&hm=b62b495cf55715bcf0ff38a86ee1ddd0a80790d8efc48d404c97f2a45eb517df&',
+      'https://cdn.discordapp.com/attachments/1419839079222218772/1430527475972182046/images_9.jpg?ex=68fa1a08&is=68f8c888&hm=570cd3275fd057a898cb95737802f5ec6201346d0319a0b03e0d71f06e29468d&'
+    ],
+    description: 'Bola futsal berkualitas tinggi, cocok untuk pertandingan indoor maupun Outdoor'',
+  },
+  {
+    id: 2,
+    name: 'Bola Sepak Classic',
+    price: {
+      4: 159000,
+      5: 209000,
+      6: 259000,
+    },
+    images: [
+      'https://cdn.discordapp.com/attachments/1419839079222218772/1430507364842344540/bola-futsal-ukuran-4-diameter-63-cm-merah-putih-kipsta-8773833.jpg?ex=68fa074d&is=68f8b5cd&hm=dca60b39bce47e73b4b8eed3b471849d0716e0066c9e514dbf639b029e87871b&',
+      'https://cdn.discordapp.com/attachments/1419839079222218772/1430525330862637176/images_4.jpg?ex=68fa1808&is=68f8c688&hm=b480d955498a13276b7baa5a1d40bfda35c2bbb7c066237a83b4f669cef0a75a&',
+      'https://cdn.discordapp.com/attachments/1419839079222218772/1430525330862637176/images_4.jpg?ex=68fa1808&is=68f8c688&hm=b480d955498a13276b7baa5a1d40bfda35c2bbb7c066237a83b4f669cef0a75a&'
+    ],
+    description: 'Bola sepak standar FIFA, bahan kulit sintetis premium.',
+  },
+  {
+    id: 3,
+    name: 'Bola Sepak Training',
+    price: {
+      4: 99000,
+      5: 129000,
+      6: 149000,
+    },
+    images: [
+      'https://cdn.discordapp.com/attachments/1419839079222218772/1430528587651350618/images_12.jpg?ex=68fa1b11&is=68f8c991&hm=92e6e2c4c9b09bb1205368e2d4732fbb735b1adf9f3c14ff105b120f20372fb0&',
+      'https://cdn.discordapp.com/attachments/1419839079222218772/1430528588062654505/images_13.jpg?ex=68fa1b11&is=68f8c991&hm=c49ec9250f9da8b78949b589d18616f853e6fafa07fdf00219bbabb4f2739d87&',
+      'https://cdn.discordapp.com/attachments/1419839079222218772/1430528588481953905/images_14.jpg?ex=68fa1b11&is=68f8c991&hm=97f1fc5dd5e374e1abdb98fce3e8ba1dd88f0175ddbf3a8c6ee9335e5f760445&'
+    ],
+    description: 'Bola sepak outdoor yang cocok untuk melakukan latihan dribbling dengan lancar.',
+  },
+];
